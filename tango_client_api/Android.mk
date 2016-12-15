4e95834eaa58ec9d1de6072c2938c05b851b26a2
Edward Zhang <edzhang@cs.washington.edu>
2016-12-15 02:21:05 -0800

    Update tango client api

--- tango_client_api/Android.mk
+++ tango_client_api/Android.mk
@@ -10,9 +10,21 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := tango_client_api
-LOCAL_EXPORT_LDLIBS := -L$(LOCAL_PATH)/lib \
-                       -ltango_client_api
+
+
+ifeq ($(TARGET_ARCH), x86)
+  LOCAL_EXPORT_LDLIBS := -L$(LOCAL_PATH)/lib/x86 -ltango_client_api
+endif
+
+ifeq ($(TARGET_ARCH), arm64)
+  LOCAL_EXPORT_LDLIBS := -L$(LOCAL_PATH)/lib/arm64-v8a -ltango_client_api
+endif
+
+ifeq ($(TARGET_ARCH), arm)
+  LOCAL_EXPORT_LDLIBS := -L$(LOCAL_PATH)/lib/armeabi-v7a -ltango_client_api
+endif
+
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_SRC_FILES := $(LOCAL_PATH)/lib/libtango_client_stub.a
+LOCAL_SRC_FILES := lib/libtango_client_stub.a
 
 include $(PREBUILT_STATIC_LIBRARY)
